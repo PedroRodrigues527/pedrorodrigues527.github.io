0c2950dce37ff16baef30be30a2974029a85c176
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2026-01-10 16:12:01 +0000

    Dynamic text for projects and general overview

---
+++ res/journey.js
@@ -0,0 +1,68 @@
+const JOURNEY = [
+  {
+    "company": "Tech4Home",
+    "role": "Backend Developer",
+    "time": "19/05/2025 - Present",
+    "details": null
+  },
+  {
+    "company": "Acin",
+    "role": "Fullstack Development",
+    "time": "14/11/2023 - 14/04/2025",
+    "details": null
+  },
+  {
+    "company": "Started Master's Degree in Computer Science",
+    "role": null,
+    "time": "09/2023",
+    "details": null
+  },
+  {
+    "company": "VipaConnect",
+    "role": "Fullstack Development",
+    "time": "01/08/2022 - 30/06/2023",
+    "details": {
+      "projects": ["GovGo", "SeeApp"],
+      "tools": "PHP (+Laravel), HTML, CSS, Javascript (React + React Admin), Postgres"
+    }
+  },
+  {
+    "company": "SeeHealth",
+    "role": "Fullstack Development",
+    "time": "01/07/2022 - 01/08/2022 (internship)",
+    "details": {
+      "projects": ["SeeApp"],
+      "tools": "PHP (+Laravel), HTML, CSS, Javascript (React + React Admin), Postgres"
+    }
+  },
+  {
+    "company": "Finished Computer Science and Engineering",
+    "role": null,
+    "time": "01/07/2022",
+    "details": null
+  },
+  {
+    "company": "Fundamentals of digital marketing",
+    "role": null,
+    "time": "11/02/2022",
+    "details": null
+  },
+  {
+    "company": "NDG Linux Essentials Certification",
+    "role": null,
+    "time": "07/12/2021",
+    "details": null
+  },
+  {
+    "company": "NDG Linux Unhatched Certification",
+    "role": null,
+    "time": "20/10/2021",
+    "details": null
+  },
+  {
+    "company": "Started Computer Science and Engineering",
+    "role": null,
+    "time": "01/09/2019",
+    "details": null
+  }
+];
---
+++ res/projects.js
@@ -0,0 +1,39 @@
+const PROJECTS = [
+    {
+        "title": "Nodejs_file_storage",
+        "description": "It's a simple node.js local webserver, that allows the local machine to store and manage files in the 'uploads' folder. Those files in this folder can be accessed by other devices, in LAN.",
+        "languagesUsed": "Javascript (node.js)",
+        "url": "https://github.com/PedroRodrigues527/Nodejs_file_storage"
+    },
+    {
+        "title": "Primitive Canvas",
+        "description": "(Academic project) Using Webgl, to do 3d movement (rotation, translation movement, lightning), to random object, like cubes, to some models.",
+        "languagesUsed": "Javascript (webgl)",
+        "url": "https://github.com/PedroRodrigues527/Primitive_Canvas"
+    },
+    {
+        "title": "HTTP Server JAVA",
+        "description": "(Academic project) Java http server, with multithreading and parallel programing.",
+        "languagesUsed": "Java",
+        "url": "https://github.com/PedroRodrigues527/HTTP_Server_Java"
+    },
+    {
+        "title": "Madeira-Weather",
+        "description": "Simple webscrapping project using python (Beautiful Soup) to retrieve weather information (about madeira island).",
+        "languagesUsed": "Python (+ Beautiful Soup)",
+        "url": "https://github.com/PedroRodrigues527/Madeira-Weather-Python-web_scraper"
+    },
+    {
+        "title": "TLS CHAT",
+        "description": "(Academic project) Java TLS chat, with multiple encryptions. Eg: Symmetric encryption (DES, 3DES, AES), Asymmetric encryption (RSA).",
+        "languagesUsed": "Java",
+        "url": "https://github.com/PedroRodrigues527/HTTP_Server_Java"
+    },
+    {
+        "title": "Discord_bot_Roberto",
+        "description": "Roberto helps with your most difficult choices and responds to your questions in Portuguese. It also plays Rock Paper Scissors.",
+        "languagesUsed": "Javascript (node.js + discord.js), Python (Beautiful Soup)",
+        "url": "https://github.com/PedroRodrigues527/Discord_bot_Roberto"
+    }
+    
+]
