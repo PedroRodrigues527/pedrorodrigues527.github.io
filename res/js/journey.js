6ba856d57e853540ca9d30bb3b6f5c16a30a24cd
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2026-05-10 13:57:45 +0100

    Update journey.js

--- res/js/journey.js
+++ res/js/journey.js
@@ -2,7 +2,7 @@ const JOURNEY = [
   {
     "company": "Tech4Home",
     "role": "Backend Developer",
-    "time": "19/05/2025 - Present",
+    "time": "05/2025 - Present",
     "details": {
       "tools": "Java and C++ applications"
     }
@@ -10,7 +10,7 @@ const JOURNEY = [
   {
     "company": "Acin",
     "role": "Fullstack Development",
-    "time": "14/11/2023 - 14/04/2025",
+    "time": "11/2023 - 04/2025",
     "details": {
       "projects": ["IDok", "SIMplifica"],
       "tools": "PHP (+ Laravel and CodeIgniter), HTML, CSS, Javascript/Typescript, SQL databases"
@@ -25,7 +25,7 @@ const JOURNEY = [
   {
     "company": "VipaConnect",
     "role": "Fullstack Development",
-    "time": "01/08/2022 - 30/06/2023",
+    "time": "08/2022 - 06/2023",
     "details": {
       "projects": ["GovGo", "SeeApp"],
       "tools": "PHP (+Laravel), HTML, CSS, Javascript (React + React Admin), Postgres"
@@ -34,40 +34,16 @@ const JOURNEY = [
   {
     "company": "SeeHealth",
     "role": "Fullstack Development",
-    "time": "01/07/2022 - 01/08/2022 (internship)",
+    "time": "07/2022 - 08/2022 (internship)",
     "details": {
       "projects": ["SeeApp"],
       "tools": "PHP (+Laravel), HTML, CSS, Javascript (React + React Admin), Postgres"
     }
   },
   {
-    "company": "Finished Computer Science and Engineering",
+    "company": "Computer Science and Engineering",
     "role": null,
-    "time": "01/07/2022",
-    "details": null
-  },
-  {
-    "company": "Fundamentals of digital marketing",
-    "role": null,
-    "time": "11/02/2022",
-    "details": null
-  },
-  {
-    "company": "NDG Linux Essentials Certification",
-    "role": null,
-    "time": "07/12/2021",
-    "details": null
-  },
-  {
-    "company": "NDG Linux Unhatched Certification",
-    "role": null,
-    "time": "20/10/2021",
-    "details": null
-  },
-  {
-    "company": "Started Computer Science and Engineering",
-    "role": null,
-    "time": "01/09/2019",
+    "time": "09/2019 - 07/2022",
     "details": null
   }
 ];
